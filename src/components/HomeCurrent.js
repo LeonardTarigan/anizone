81d005fec154248a1d386ffd7be82ea30529a856
LeonardTarigan <leontrg32@gmail.com>
2022-10-02 10:37:19 +0700

    solved ongoing page bug

--- src/components/HomeCurrent.js
+++ src/components/HomeCurrent.js
@@ -23,34 +23,29 @@ function HomeCurrent() {
             <h2 className='mb-5 text-lg font-semibold'>Current Season</h2>
             <div className='flex flex-wrap justify-center gap-4 md:justify-start'>
                 {animeList &&
-                    animeList
-                        .filter((anime) => {
-                            return anime.airing === true;
-                        })
-                        .slice(0, 10)
-                        .map((anime) => {
-                            const { title, mal_id } = anime;
-                            const { large_image_url } = anime.images.jpg;
+                    animeList.slice(0, 10).map((anime) => {
+                        const { title, mal_id } = anime;
+                        const { large_image_url } = anime.images.jpg;
 
-                            return (
-                                <Link
-                                    key={mal_id}
-                                    to={`/anime/${mal_id}/${title
-                                        .split(' ')
-                                        .join('_')}`}
-                                    className='group relative h-fit w-fit cursor-pointer overflow-hidden rounded-md bg-zinc-100'
-                                >
-                                    <img
-                                        src={large_image_url}
-                                        alt={title}
-                                        className='h-52 w-36 md:h-40 md:w-32'
-                                    />
-                                    <p className='absolute -bottom-[4.4rem] left-0 h-28 w-40 bg-zinc-900 bg-opacity-80 p-2 text-[0.65rem] text-white transition-all duration-300 group-hover:bottom-0 md:w-32'>
-                                        {title}
-                                    </p>
-                                </Link>
-                            );
-                        })}
+                        return (
+                            <Link
+                                key={mal_id}
+                                to={`/anime/${mal_id}/${title
+                                    .split(' ')
+                                    .join('_')}`}
+                                className='group relative h-fit w-fit cursor-pointer overflow-hidden rounded-md bg-zinc-100'
+                            >
+                                <img
+                                    src={large_image_url}
+                                    alt={title}
+                                    className='h-52 w-36 md:h-40 md:w-32'
+                                />
+                                <p className='absolute -bottom-[4.4rem] left-0 h-28 w-40 bg-zinc-900 bg-opacity-80 p-2 text-[0.65rem] text-white transition-all duration-300 group-hover:bottom-0 md:w-32'>
+                                    {title}
+                                </p>
+                            </Link>
+                        );
+                    })}
             </div>
             <div className='mt-3 flex w-full justify-center md:justify-end'>
                 <button className='cursor-pointer text-xs transition-all duration-200 hover:text-rose-500'>
